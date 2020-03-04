e01eef5ef60468d58f14476336ad6a29e3c4ca42
Wisse <wissehes@gmail.com>
2020-03-04 23:01:18 +0100

    ja

--- components/Danke.vue
+++ components/Danke.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Danke!</h1>
+  <div>
+    <h1>Danke!</h1>
+    <p><router-link to="/">Zurück</router-link></p>
+  </div>
 </template>
+
+<style scoped>
+div {
+  margin: auto;
+  border-radius: 10px;
+  padding: 10px 10px;
+  margin-top: 4rem;
+  padding-bottom: 4rem;
+  width: 50%;
+  background: radial-gradient(
+    circle,
+    rgba(0, 179, 255, 1) 0%,
+    rgba(0, 93, 255, 1) 100%
+  );
+  color: white;
+  text-align: center;
+}
+a {
+  color: black;
+}
+</style>
