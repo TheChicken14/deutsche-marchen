09962cbe9b50f6b494965cf8f8ed2343b1447242
Wisse <wissehes@gmail.com>
2020-03-05 15:56:57 +0100

    new style

--- components/Marchen.vue
+++ components/Marchen.vue
@@ -1,56 +1,62 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-8 text">
-        <h1>{{ marchen.name }}</h1>
-        <p v-if="marchen.text.text">{{ marchen.text.text }}</p>
-        <p v-else>
-          Aut ut placeat rerum rerum ducimus modi sequi. Et et quae qui. Ipsam
-          voluptatem culpa omnis. Est rerum libero quam omnis veniam enim.
-          Debitis rerum repellendus commodi accusamus ducimus doloremque.
-          Dolorem sit facilis ut voluptatem. Eos perspiciatis et autem. Animi in
-          rem similique libero autem. At ut magnam quo ut ab laudantium nemo
-          unde. Dolor similique nostrum at. Aliquam temporibus dicta quas dicta
-          ut eaque magni. Autem in voluptates expedita porro. Quasi sunt eaque
-          accusantium sed quia incidunt. Error eum exercitationem tempora
-          eligendi. Unde aut vitae perferendis nulla blanditiis. Omnis quam et
-          aliquid qui. Eligendi asperiores aut iusto beatae architecto. Numquam
-          minima tempore minima sed. Beatae et animi ab repudiandae magnam
-          assumenda et. Et minus nesciunt labore nihil placeat occaecati ut.
-          Occaecati voluptates perspiciatis asperiores aut laborum nisi cumque.
-          Iure commodi nobis doloremque aliquam sit. Alias in nam dicta dolor
-          inventore est. Illo et amet autem doloribus hic. Aut ut placeat rerum
-          rerum ducimus modi sequi. Et et quae qui. Ipsam voluptatem culpa
-          omnis. Est rerum libero quam omnis veniam enim. Debitis rerum
-          repellendus commodi accusamus ducimus doloremque. Dolorem sit facilis
-          ut voluptatem. Eos perspiciatis et autem. Animi in rem similique
-          libero autem. At ut magnam quo ut ab laudantium nemo unde. Dolor
-          similique nostrum at. Aliquam temporibus dicta quas dicta ut eaque
-          magni. Autem in voluptates expedita porro. Quasi sunt eaque
-          accusantium sed quia incidunt. Error eum exercitationem tempora
-          eligendi. Unde aut vitae perferendis nulla blanditiis. Omnis quam et
-          aliquid qui. Eligendi asperiores aut iusto beatae architecto. Numquam
-          minima tempore minima sed. Beatae et animi ab repudiandae magnam
-          assumenda et. Et minus nesciunt labore nihil placeat occaecati ut.
-          Occaecati voluptates perspiciatis asperiores aut laborum nisi cumque.
-          Iure commodi nobis doloremque aliquam sit. Alias in nam dicta dolor
-          inventore est. Illo et amet autem doloribus hic. Aut ut placeat rerum
-          rerum ducimus modi sequi. Et et quae qui. Ipsam voluptatem culpa
-          omnis. Est rerum libero quam omnis veniam enim. Debitis rerum
-          repellendus commodi accusamus ducimus doloremque. Dolorem sit facilis
-          ut voluptatem. Eos perspiciatis et autem. Animi in rem similique
-          libero autem. At ut magnam quo ut ab laudantium nemo unde. Dolor
-          similique nostrum at. Aliquam temporibus dicta quas dicta ut eaque
-          magni. Autem in voluptates expedita porro. Quasi sunt eaque
-          accusantium sed quia incidunt. Error eum exercitationem tempora
-          eligendi. Unde aut vitae perferendis nulla blanditiis. Omnis quam et
-          aliquid qui. Eligendi asperiores aut iusto beatae architecto. Numquam
-          minima tempore minima sed. Beatae et animi ab repudiandae magnam
-          assumenda et. Et minus nesciunt labore nihil placeat occaecati ut.
-          Occaecati voluptates perspiciatis asperiores aut laborum nisi cumque.
-          Iure commodi nobis doloremque aliquam sit. Alias in nam dicta dolor
-          inventore est. Illo et amet autem doloribus hic.
-        </p>
+      <div class="col-md-8 left">
+        <div class="name">
+          <h1>{{ marchen.name }}</h1>
+          <h4>{{ marchen.kinder }}</h4>
+        </div>
+        <div class="text">
+          <p v-if="marchen.text.text">{{ marchen.text.text }}</p>
+          <p v-else>
+            Aut ut placeat rerum rerum ducimus modi sequi. Et et quae qui. Ipsam
+            voluptatem culpa omnis. Est rerum libero quam omnis veniam enim.
+            Debitis rerum repellendus commodi accusamus ducimus doloremque.
+            Dolorem sit facilis ut voluptatem. Eos perspiciatis et autem. Animi
+            in rem similique libero autem. At ut magnam quo ut ab laudantium
+            nemo unde. Dolor similique nostrum at. Aliquam temporibus dicta quas
+            dicta ut eaque magni. Autem in voluptates expedita porro. Quasi sunt
+            eaque accusantium sed quia incidunt. Error eum exercitationem
+            tempora eligendi. Unde aut vitae perferendis nulla blanditiis. Omnis
+            quam et aliquid qui. Eligendi asperiores aut iusto beatae
+            architecto. Numquam minima tempore minima sed. Beatae et animi ab
+            repudiandae magnam assumenda et. Et minus nesciunt labore nihil
+            placeat occaecati ut. Occaecati voluptates perspiciatis asperiores
+            aut laborum nisi cumque. Iure commodi nobis doloremque aliquam sit.
+            Alias in nam dicta dolor inventore est. Illo et amet autem doloribus
+            hic. Aut ut placeat rerum rerum ducimus modi sequi. Et et quae qui.
+            Ipsam voluptatem culpa omnis. Est rerum libero quam omnis veniam
+            enim. Debitis rerum repellendus commodi accusamus ducimus
+            doloremque. Dolorem sit facilis ut voluptatem. Eos perspiciatis et
+            autem. Animi in rem similique libero autem. At ut magnam quo ut ab
+            laudantium nemo unde. Dolor similique nostrum at. Aliquam temporibus
+            dicta quas dicta ut eaque magni. Autem in voluptates expedita porro.
+            Quasi sunt eaque accusantium sed quia incidunt. Error eum
+            exercitationem tempora eligendi. Unde aut vitae perferendis nulla
+            blanditiis. Omnis quam et aliquid qui. Eligendi asperiores aut iusto
+            beatae architecto. Numquam minima tempore minima sed. Beatae et
+            animi ab repudiandae magnam assumenda et. Et minus nesciunt labore
+            nihil placeat occaecati ut. Occaecati voluptates perspiciatis
+            asperiores aut laborum nisi cumque. Iure commodi nobis doloremque
+            aliquam sit. Alias in nam dicta dolor inventore est. Illo et amet
+            autem doloribus hic. Aut ut placeat rerum rerum ducimus modi sequi.
+            Et et quae qui. Ipsam voluptatem culpa omnis. Est rerum libero quam
+            omnis veniam enim. Debitis rerum repellendus commodi accusamus
+            ducimus doloremque. Dolorem sit facilis ut voluptatem. Eos
+            perspiciatis et autem. Animi in rem similique libero autem. At ut
+            magnam quo ut ab laudantium nemo unde. Dolor similique nostrum at.
+            Aliquam temporibus dicta quas dicta ut eaque magni. Autem in
+            voluptates expedita porro. Quasi sunt eaque accusantium sed quia
+            incidunt. Error eum exercitationem tempora eligendi. Unde aut vitae
+            perferendis nulla blanditiis. Omnis quam et aliquid qui. Eligendi
+            asperiores aut iusto beatae architecto. Numquam minima tempore
+            minima sed. Beatae et animi ab repudiandae magnam assumenda et. Et
+            minus nesciunt labore nihil placeat occaecati ut. Occaecati
+            voluptates perspiciatis asperiores aut laborum nisi cumque. Iure
+            commodi nobis doloremque aliquam sit. Alias in nam dicta dolor
+            inventore est. Illo et amet autem doloribus hic.
+          </p>
+        </div>
       </div>
       <div class="col-md-4 sidebar">
         <img v-if="marchen.image" class="image" v-bind:src="marchen.image" />
@@ -73,9 +79,9 @@ module.exports = {
   },
   mounted() {
     if (this.marchens) {
-      this.found = this.marchens.find(e => e.id == this.$route.params.id);
-      this.marchen = this.found
-        ? this.found
+      found = this.marchens.find(e => e.id == this.$route.params.id);
+      this.marchen = found
+        ? found
         : { name: "Nicht gefunden!", id: 0, text: {} };
     } else {
       axios.get("/marchen.json").then(({ data }) => {
@@ -89,10 +95,12 @@ module.exports = {
   },
   beforeRouteUpdate(to, from, next) {
     if (this.marchens) {
-      this.found = this.marchens.find(e => e.id == this.$route.params.id);
-      this.marchen = this.found
-        ? this.found
-        : { name: "Nicht gefunden!", id: 0, text: {} };
+      setTimeout(() => {
+        this.found = this.marchens.find(e => e.id == this.$route.params.id);
+        this.marchen = this.found
+          ? this.found
+          : { name: "Nicht gefunden!", id: 0, text: {} };
+      }, 100);
     } else {
       axios.get("/marchen.json").then(({ data }) => {
         this.found = data.find(e => e.id == this.$route.params.id);
@@ -111,7 +119,6 @@ module.exports = {
 @media (max-width: 768px) {
   .sidebar {
     margin-top: 3rem;
-    border-top: 5px solid rgb(49, 49, 49);
   }
   .image {
     height: 18rem;
@@ -120,23 +127,33 @@ module.exports = {
 @media (min-width: 768px) {
   .sidebar {
     padding-top: 1rem;
-    border-left: 5px solid black;
   }
   .image {
     height: 20rem;
   }
 }
 
-.row {
+.sidebar {
   background-color: white;
   border-radius: 10px;
+  margin-top: 2rem;
 }
-p {
+
+.text {
+  padding: 2rem;
+  background-color: white;
+  border-radius: 10px;
+  margin-top: 10px;
+}
+/*p {
   margin: 2rem;
   margin-bottom: 3rem;
-}
-h1 {
+}*/
+.name {
+  background-color: white;
+  border-radius: 10px;
   margin-top: 2rem;
+  padding: 0.5rem;
   text-align: center;
 }
 .image {
